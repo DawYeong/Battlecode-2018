588eae7ccc6fd73e75e138f829728374c076c7c8
Eric Schilha <EricSchilha@users.noreply.github.com>
2018-01-11 19:05:06 -0500

    rockets build and launch

--- Player/Player.java
+++ Player/Player.java
@@ -1,49 +1,86 @@
 import bc.*;
 
+import java.util.*;
+import java.io.*;
+
 public class Player {
-    static GameController gc = new GameController();
-    static Team myTeam = gc.team();
+    public static GameController gc = new GameController();
+    public static Team myTeam = gc.team();
+    public static MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
+    public static Direction[] directions = Direction.values();
+    public static VecUnit units;
+
     public static void main(String[] args) {
-        MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
-        Direction[] directions = Direction.values();
-        boolean builtRocket = false;
         System.out.println(myTeam);
         gc.queueResearch(UnitType.Rocket);
         while (true) {
-            System.out.println("Current round: "+gc.round());
-            VecUnit units = gc.myUnits();
-
+            System.out.println("Current round: " + gc.round());
+            units = gc.myUnits();
             for (int i = 0; i < units.size(); i++) {
                 Unit unit = units.get(i);
-                VecUnit nearbyRockets = senseNearbyUnitsByType(unit.location(), 1, UnitType.Rocket);
-                if(gc.canBlueprint(unit.id(), UnitType.Rocket, directions[0]) && !builtRocket){
-                    gc.blueprint(unit.id(), UnitType.Rocket, directions[0]);
-                    builtRocket = true;
+                switch (unit.unitType()) {
+                    case Worker: //We don't need UnitType.Worker since it is an enum
+                        runWorker(unit);
+                        break;
+                    case Factory:
+                        break;
+                    case Rocket:
+                        Random rand = new Random();
+                        int nX = rand.nextInt(10) + 5;
+                        int nY = rand.nextInt(10) + 5;
+                        VecUnitID unitsInRocket = unit.structureGarrison();
+                        if (gc.canLaunchRocket(unit.id(), new MapLocation(Planet.Mars, nX, nY)) && unitsInRocket.size()>0) {
+                            gc.launchRocket(unit.id(), new MapLocation(Planet.Mars, nX, nY));
+                            System.out.println("BLASTOFF!");
+                        }
+                        break;
                 }
-                for(int j = 0; j < nearbyRockets.size(); j++) {
-                    if (gc.canBuild(unit.id(), UnitType.Rocket) && builtRocket) {
-                        gc.build(unit.id(), nearbyRockets.get(j).id());
-                        System.out.println("BUILT ROCKET");
-                    }
-                }
-//                if(unit.unitType()==UnitType.Worker){
-//                    for(int j = 0; j < 8; j++){
-//                        if(gc.canBlueprint(unit.id(), UnitType.Factory, directions[j])){
-//                            gc.blueprint(unit.id(), UnitType.Factory, directions[j]);
-//                            continue;
-//                        }
-//                        if(gc.canReplicate(unit.id(), directions[j])){
-//                            gc.replicate(unit.id(), directions[j]);
-//                            continue;
-//                        }
-//                        if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), directions[j])) {
-//                            gc.moveRobot(unit.id(), directions[j]);
-//                        }
-//                    }
-//                }
                 units = gc.myUnits();
             }
             gc.nextTurn();
         }
     }
+
+    public static void runWorker(Unit unit) {
+        VecUnit nearbyRockets = gc.senseNearbyUnitsByType(unit.location().mapLocation(), 1, UnitType.Rocket);
+        if (gc.canBlueprint(unit.id(), UnitType.Rocket, directions[0])) {
+            gc.blueprint(unit.id(), UnitType.Rocket, directions[0]);
+        }
+        for (int i = 0; i < nearbyRockets.size(); i++) {
+            if (nearbyRockets.get(i).structureIsBuilt() == 0) {//0 false, 1 true?
+                if (gc.canBuild(unit.id(), nearbyRockets.get(i).id())) {
+                    gc.build(unit.id(), nearbyRockets.get(i).id());
+                }
+            } else if (nearbyRockets.get(i).structureIsBuilt() == 1) {
+                if (gc.canLoad(nearbyRockets.get(i).id(), unit.id())) {
+                    gc.load(nearbyRockets.get(i).id(), unit.id());
+                }
+            }
+
+//            PlanetMap map = new PlanetMap();
+            /*Random rand = new Random();
+//            int nX = rand.nextInt((int) map.getWidth());
+//            int nY = rand.nextInt((int) map.getWidth());
+            int nX = rand.nextInt(20);
+            int nY = rand.nextInt(20);
+            VecUnitID unitsInRocket = nearbyRockets.get(i).structureGarrison();
+            if (gc.canLaunchRocket(nearbyRockets.get(i).id(), new MapLocation(Planet.Mars, nX, nY)) && unitsInRocket.size()>0) {
+                gc.launchRocket(nearbyRockets.get(i).id(), new MapLocation(Planet.Mars, nX, nY));
+                System.out.println("BLASTOFF!");
+            }*/
+        }
+    }
+//        for (int i = 0; i < 8; i++) {
+//            if (gc.canBlueprint(unit.id(), UnitType.Factory, directions[i])) {
+//                gc.blueprint(unit.id(), UnitType.Factory, directions[i]);
+//                continue;
+//            }
+//            if (gc.canReplicate(unit.id(), directions[i])) {
+//                gc.replicate(unit.id(), directions[i]);
+//                continue;
+//            }
+//            if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), directions[i])) {
+//                gc.moveRobot(unit.id(), directions[i]);
+//            }
+//        }
 }
